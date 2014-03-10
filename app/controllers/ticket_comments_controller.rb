d57c27a2b48822eed0ffcb6ac93c221327a4d2f9
Alexander Shkatov <o.shkatov@live.ru>
2014-03-10 13:52:24 +0200

    Mail tpls Send mail on added comment

--- app/controllers/ticket_comments_controller.rb
+++ app/controllers/ticket_comments_controller.rb
@@ -3,6 +3,9 @@ class TicketCommentsController < ApplicationController
     ticket = Ticket.find(params[:ticket_id])
     comment = ticket.ticket_comments.new(comments_params)
     ticket.user_id = comment.user_id = session[:current_user_id]
+    unless ticket.user_id.nil? #staff logged and type in comment, notify ticket creator
+      TicketNotify.changes_in_ticket(ticket,comment)
+    end
     ticket.save
     flash[:error] = 'Type comment before send it' unless comment.save
     redirect_to ticket_uid_path(ticket.uid)
--- app/mailers/ticket_notify.rb
+++ app/mailers/ticket_notify.rb
@@ -2,9 +2,11 @@ class TicketNotify < ActionMailer::Base
   default from: "cmb_5@ukr.net"
   def new_ticket(ticket)
     @ticket = ticket
-    mail(to: @ticket.customer_email, subject: 'You created new ticket')
+    mail(to: @ticket.customer_email, subject: 'You create new ticket '+ticket.uid.to_s)
   end
-  def changes_in_ticket(ticket)
-    
+  def changes_in_ticket(ticket,comment)
+    @ticket = ticket
+    @comment = comment
+    mail(to: @ticket.customer_email, subject: 'New comment in ticket '+ticket.uid.to_s)
   end
 end
